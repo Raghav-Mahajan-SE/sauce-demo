1b2eac38e584f19e1350569ef6b2442f5289084a
Raghav Mahajan <44345836+Raghav-Mahajan-SE@users.noreply.github.com>
2026-07-06 22:17:07 +0530

    added config file and its reader code

--- src/main/java/com/saucedemo/config/ConfigReader.java
+++ src/main/java/com/saucedemo/config/ConfigReader.java
@@ -1,4 +1,24 @@
 package com.saucedemo.config;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    private static Properties prop;
+
+    public static String get(String key){
+
+        try {
+            FileInputStream file = new FileInputStream("src/test/java/com/saucedemo/resources/data/config.properties");
+            prop = new Properties();
+            prop.load(file);
+        }catch(Exception e){
+            throw new RuntimeException("config.properties is not loaded: " + e.getMessage());
+        }
+        String sysValue = System.getProperty(key);
+        return (sysValue!=null)?sysValue:prop.getProperty(key);
+
+    }
 }
+
