be930d98df3ec788d1e2c406df54d79b21f045fb
Raghav Mahajan <44345836+Raghav-Mahajan-SE@users.noreply.github.com>
2026-07-09 23:11:31 +0530

    fix prallel testing

--- src/main/java/com/saucedemo/config/ConfigReader.java
+++ src/main/java/com/saucedemo/config/ConfigReader.java
@@ -5,20 +5,20 @@ import java.util.Properties;
 
 public class ConfigReader {
 
-    private static Properties prop;
+    private static final Properties prop = new Properties();
 
-    public static String get(String key){
-
+    static {
         try {
             FileInputStream file = new FileInputStream("src/test/java/com/saucedemo/resources/data/config.properties");
-            prop = new Properties();
             prop.load(file);
         }catch(Exception e){
             throw new RuntimeException("config.properties is not loaded: " + e.getMessage());
         }
+    }
+
+    public static String get(String key){
         String sysValue = System.getProperty(key);
         return (sysValue!=null)?sysValue:prop.getProperty(key);
-
     }
 }
 
